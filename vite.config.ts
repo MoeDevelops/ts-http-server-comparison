bd34811e6f7cd9252c6108518a584feed0f9527c
MoeDev <143090643+MoeDevelops@users.noreply.github.com>
2026-03-03 23:18:35 +0100

    Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,30 @@
 import tailwindcss from "@tailwindcss/vite"
 import devtools from "solid-devtools/vite"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 import solidPlugin from "vite-plugin-solid"
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin(), tailwindcss()],
+  plugins: [
+    devtools(),
+    solidPlugin(),
+    tailwindcss(),
+    VitePWA({
+      manifest: {
+        name: "Cool App",
+        short_name: "Cool App",
+        description: "A very cool app",
+        theme_color: "#202020",
+        icons: [
+          {
+            src: "/favicon-8x.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     port: 4000,
   },
